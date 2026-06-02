fb3f264aa9fcaa5fc3e5b5065e7f977e23a1d4aa
mustafa-awartani2 <mustafaawartani49@gmail.com>
2026-06-02 12:19:42 +0300

    define method  status_pie

--- visualizer.py
+++ visualizer.py
@@ -20,3 +20,25 @@ class Visualizer:
         plt.ylabel("Number of Appointments")
 
         plt.show()
+
+    def status_pie(self, appointments):
+
+        counts = {}
+
+        for a in appointments:
+            if a["status"] in counts:
+                counts[a["status"]] += 1
+            else:
+                counts[a["status"]] = 1
+
+        plt.figure(figsize=(7, 7))
+
+        plt.pie(
+            counts.values(),
+            labels=counts.keys(),
+            autopct="%1.1f%%"
+        )
+
+        plt.title("Appointment Status Distribution")
+
+        plt.show()
